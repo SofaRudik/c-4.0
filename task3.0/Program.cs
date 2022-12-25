548a6d4d880517bffb54c660d14eb8b2e1b2b05e
sofia <sonrud@mail.ru>
2022-12-25 16:27:31 +0300

    add tast29

---
+++ task3.0/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void numbers(int [] array);
+{
+    for (int i=0;i<array.Length;i++)
+    Console.WriteLine($"Введите {i}-ое число: ");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.Clear();
+int [] array= new int [8];
+numbers(array);
+Console.WriteLine($"[{string.Join(",",array)}]");
+
+
