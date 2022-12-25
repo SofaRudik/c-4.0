356845a12aba6003b5e77f83df68fc6d41f2e2a3
sofia <sonrud@mail.ru>
2022-12-25 15:47:05 +0300

    add task27

---
+++ task2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while(n>0)
+{
+    int m=n%10;
+    n=n/10;
+    sum=sum+m;
+
+}
+Console.WriteLine(sum);
+
